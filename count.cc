cceac8f418098d02d99318e56586e06cf951fa43
pjh287231 <pjh287231@naver.com>
2023-09-05 20:05:32 +0900

    count.cc

---
+++ count.cc
@@ -0,0 +1,12 @@
+#include "count.h"
+
+using namespace std;
+
+void count_number() {
+
+	for(int i=1; i<=10; i++)
+		cout<<i<<"\n";
+	cout<<"2019311052";
+    // lab2 //
+    // do it yourself //
+}
